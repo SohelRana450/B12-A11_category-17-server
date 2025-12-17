2a568d02f5f547c218c90d3e42263e75d793cbab
SohelRana450 <msr945071337@gmail.com>
2025-12-17 21:54:23 +0600

    fourth commit

--- index.js
+++ index.js
@@ -47,7 +47,12 @@ async function run() {
         res.send(result)
     })
 
-   
+    app.get('/all-tickets/:id', async(req,res)=>{
+        const id = req.params.id
+        const query = {_id: new ObjectId(id)}
+        const result = await ticketCollection.findOne(query)
+        res.send(result)
+    })
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     // Send a ping to confirm a successful connection
